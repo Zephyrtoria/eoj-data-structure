4ac5223fa42c2643221c25aaf8fa693752fb42c4
Zephyrtoria <2594379136@qq.com>
2024-12-28 19:55:10 +0800

    2024/12/28

--- homework/a1070/solve.cpp
+++ homework/a1070/solve.cpp
@@ -2,9 +2,64 @@
 #include <vector>
 using namespace std;
 
-bool isBST(int* arr, int l, int r) {
-    
-}
+typedef struct Node {
+    int val;
+    Node* left;
+    Node* right;
+    Node(int val_) {
+        val = val_;
+        left = nullptr;
+        right = nullptr;
+    }
+} Node;
+
+class Tree {
+private:
+    int n;
+    Node* root;
+    int* inorder;
+    int end;
+
+public:
+
+    Node* builder(int *arr, int cur) {
+        if (cur > n) {
+            return nullptr;
+        }
+        
+        Node* newNode = new Node(arr[cur]);
+        newNode->left = builder(arr, cur * 2);
+        newNode->right = builder(arr, cur * 2 + 1);
+        return newNode;
+    }
+
+    Tree(int n, int* arr) {
+        this->n = n;
+        root = builder(arr, 1);
+    }
+
+    void inorderTraversal(Node* cur) {
+        if (cur == nullptr) {
+            return;
+        }
+        inorderTraversal(cur->left);
+        inorder[end++] = cur->val;
+        inorderTraversal(cur->right);
+    }
+
+    bool isBST() {
+        inorder = new int[n + 1];
+        end = 1;
+
+        inorderTraversal(root);
+        for (int i = 2; i <= n; i++) {
+            if (inorder[i - 1] >= inorder[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
 
 int main() {
     int n;
@@ -14,15 +69,17 @@ int main() {
         int m;
         cin >> m;
         int* arr = new int[m];
-        for (int i = 0; i < m; i++) {
+        for (int i = 1; i <= m; i++) {
             cin >> arr[i];
         }
+        Tree t(m, arr);
 
-        if (isBST(arr, 1, 2)) {
+        if (t.isBST()) {
             cout << "Yes" << endl;
         } else {
             cout << "No" << endl;
         }
+        delete arr;
     }
     return 0;
 }
