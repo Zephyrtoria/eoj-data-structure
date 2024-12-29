648cae479b751599da1517d5dbd757a707818477
Zephyrtoria <2594379136@qq.com>
2024-12-29 14:09:53 +0800

    有几个点超时

--- homework/a1069/solve.cpp
+++ homework/a1069/solve.cpp
@@ -1,35 +1,54 @@
+#include <algorithm>
 #include <iostream>
 #include <queue>
 #include <vector>
 using namespace std;
 
+bool cmp(int a, int b) {
+    return a > b;
+}
+
 int main(void) {
-    priority_queue<int, vector<int>, greater<int>> emb;
     int n, m;
     cin >> n >> m;
-    for (int i = 0; i < n; i++) {
+    if (n <= m) {
+        int* arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        sort(arr, arr + n, cmp);
+        for (int i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+        return 0;
+    }
+
+    priority_queue<int, vector<int>, greater<int>> pq;
+    for (int i = 0; i < m; i++) {
         int a;
         cin >> a;
-        if (i < m) {
-            emb.push(a);
-        } else {
-            if (a > emb.top()) {
-                emb.pop();
-                emb.push(a);
-            }
+        pq.push(a);
+    }
+
+    for (int i = m; i < n; i++) {
+        int a;
+        cin >> a;
+        if (a > pq.top()) {
+            pq.pop();
+            pq.push(a);
         }
     }
 
-    int len = min(n, m);
-    int result[len];
-    for (int i = 0; i < len; i++) {
-        result[i] = emb.top();
-        emb.pop();
+    int* result = new int[m];
+    int end = m - 1;
+    while (!pq.empty()) {
+        result[end--] = pq.top();
+        pq.pop();
     }
-    for (int i = len - 1; i >= 0; i--) {
+    for (int i = 0; i < m; i++) {
         cout << result[i] << " ";
     }
     cout << endl;
-
     return 0;
 }
